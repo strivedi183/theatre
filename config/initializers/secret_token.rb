75268da9fcdcbba81cc9ad48d688fc209d3cc219
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-03-02 14:40:52 -0500

    Created rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Theatre::Application.config.secret_token = 'bf46f8037fe67514d0f53ee297ef1ea50cfd309d37b9414cd556e222c0bb95916bc5f605341c164d830fc04f0ed7255113a489b2a59f9a51eeb7b5f8fa3f7de9'
